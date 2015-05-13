d8d436cee4057c09ee29c09c7e33900583bfe2cb
Chyld Medford <chyld.medford@gmail.com>
2015-05-12 17:23:11 -0700

    isogram

---
+++ test/plugins/endpoints/puzzles/isogram.js
@@ -0,0 +1,57 @@
+/* eslint no-unused-expressions: 0 */
+
+'use strict';
+
+var Chai = require('chai');
+var Lab = require('lab');
+var Mongoose = require('mongoose');
+var Server = require('../../../../lib/server');
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Chai.expect;
+var it = lab.test;
+var before = lab.before;
+var after = lab.after;
+
+var server;
+
+describe('GET /puzzles/isogram/{word}', function(){
+  before(function(done){
+    Server.init(function(err, srvr){
+      if(err){ throw err; }
+      server = srvr;
+      done();
+    });
+  });
+
+  after(function(done){
+    server.stop(function(){
+      Mongoose.disconnect(done);
+    });
+  });
+
+  it('should return true for dog isogram', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isogram/dog', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.be.true;
+      done();
+    });
+  });
+
+  it('should return false for ada - not isogram', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isogram/abba', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.be.false;
+      done();
+    });
+  });
+
+  it('should return false for abAB - case insensitive - not isogram', function(done){
+    server.inject({method: 'GET', url: '/puzzles/isogram/abAB', credentials: {_id: 3}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result.value).to.be.false;
+      done();
+    });
+  });
+});
